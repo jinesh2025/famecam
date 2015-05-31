ebd1efa59edf39c3dce6b6c6cdb59cb95a965401
Jinesh <jinesh2025@gmail.com>
2015-05-31 21:14:15 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
- define('DB_USER', 'u451544683_ypepy'); // db user
-define('DB_PASSWORD', 'tc@123456'); // db password 
-define('DB_DATABASE', 'u451544683_awyqu'); // database name
+ define('DB_USER', ''); // db user
+define('DB_PASSWORD', ''); // db password 
+define('DB_DATABASE', ''); // database name
 define('DB_SERVER', "localhost"); // db server
 ?>
